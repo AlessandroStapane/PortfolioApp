2bf4d4a6842285dd25d847d583acb67741620de9
Alessandro <alessandro.stapane@outlook.it>
2024-03-10 18:35:02 +0100

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Alessandro Stapane on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
